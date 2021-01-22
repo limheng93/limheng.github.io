bb279b295b51f705c3f988937edd98aa29c8b2ed
root <henglim514@gmail.com>
2021-01-22 11:21:25 +0700

    default

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// navbar menu
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// slide text
+
